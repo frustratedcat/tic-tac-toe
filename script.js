01267052021df47af844ee3ff846dc8e04ead76e
frustratedcat <jaredbrown@skiff.com>
2023-12-01 12:22:43 -0500

    Create the game board

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function GameBoard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push("");
+    }
+  }
+  console.log(board);
+}
+
+GameBoard();
