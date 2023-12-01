c61c079ebe3fd8e7b53c0a3206699857b46c1057
frustratedcat <jaredbrown@skiff.com>
2023-12-01 14:56:57 -0500

    Add functions to handle adding player selection to each cell, controlling the game, and putting game on screen

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Create the board for the game
 function GameBoard() {
   const rows = 3;
   const columns = 3;
@@ -8,10 +9,36 @@ function GameBoard() {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push("");
+      board[i].push(Cell());
     }
   }
   console.log(board);
 }
 
-GameBoard();
+// Create function to take player selection and put on board
+function Cell() {
+  let value = "";
+
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => {
+    value;
+  };
+
+  return { addToken, getValue };
+}
+
+// Create function to control the game
+function GameController() {
+  const board = GameBoard();
+}
+
+// Create function to put game on screen
+function ScreenController() {
+  const game = GameController();
+}
+
+// Run game
+ScreenController();
