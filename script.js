5afe09d9333ceb824d108afd03d8e07177a95054
frustratedcat <jaredbrown@skiff.com>
2023-12-03 14:09:48 -0500

    Update prompt to show player selections in correct location for each turn

--- script.js
+++ script.js
@@ -36,16 +36,7 @@ function GameBoard() {
     console.log(boardWithCellValues);
   };
 
-  //ANOTHER TEST FOR STUFF /////////////////////////////////////////////////////////////////////////////////////////////
-  const checkBoard = () => {
-    const anotherTest = board
-      .filter((row, column) => row[column].getValue() === 0)
-      .map((row, column) => row[column]);
-    console.log(anotherTest);
-    return anotherTest.length;
-  };
-
-  return { getBoard, dropToken, printBoard, checkBoard };
+  return { getBoard, dropToken, printBoard };
 }
 
 // Create function to take player selection and put on board
@@ -97,7 +88,9 @@ function GameController(
 
   // Play a round
   const playRound = (cell, row, column) => {
-    console.log(`Placing ${activePlayer.name}'s token in cell ${cell}`);
+    console.log(
+      `Placing ${activePlayer.name}'s ${activePlayer.token} in cell ${cell}`
+    );
     board.dropToken(row, column, activePlayer.token);
 
     switchPlayerTurn();
@@ -118,17 +111,27 @@ function GameController(
 function ScreenController() {
   console.log("start the game");
   const game = GameController();
-
   const board = game.getBoard();
 
   // Make player selection
   function makeSelection() {
     const activePlayer = game.getActivePlayer();
+
     let selectedRow;
     let selectedColumn;
     let selectedCell = Number(
       prompt(
-        `${activePlayer.name}:\nChoose a cell:\n[1][2][3]\n[4][5][6]\n[7][8][9]`
+        `${activePlayer.name}:\nChoose a cell:\n[${
+          board[0][0].getValue() === 0 ? [1] : board[0][0].getValue()
+        }][${board[0][1].getValue() === 0 ? [2] : board[0][1].getValue()}][${
+          board[0][2].getValue() === 0 ? [3] : board[0][2].getValue()
+        }]\n[${board[1][0].getValue() === 0 ? [4] : board[1][0].getValue()}][${
+          board[1][1].getValue() === 0 ? [5] : board[1][1].getValue()
+        }][${board[1][2].getValue() === 0 ? [6] : board[1][2].getValue()}]\n[${
+          board[2][0].getValue() === 0 ? [7] : board[2][0].getValue()
+        }][${board[2][1].getValue() === 0 ? [8] : board[2][1].getValue()}][${
+          board[2][2].getValue() === 0 ? [9] : board[2][2].getValue()
+        }]`
       )
     );
 
@@ -171,11 +174,20 @@ function ScreenController() {
   }
 
   // Run a turn
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      makeSelection();
+  const runTurn = () => {
+    let i = 0;
+    while (i < board.length) {
+      let j = 0;
+      while (j < board[i].length) {
+        makeSelection(board[i][j].getValue());
+        console.log(board[i][j].getValue());
+        j++;
+      }
+      i++;
     }
-  }
+  };
+
+  runTurn();
 }
 
 // Run game
