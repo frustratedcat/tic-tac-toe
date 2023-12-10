c87caa874d3091d0532f2cfd95f3a778450aa151
frustratedcat <jaredbrown@skiff.com>
2023-12-10 15:47:53 -0500

    Add player token to board

--- script.js
+++ script.js
@@ -96,6 +96,16 @@ function GameController(
 
 // Get player and computer choices
 function GetChoice() {
+  const cellBtn1 = document.getElementById("cell-1");
+  const cellBtn2 = document.getElementById("cell-2");
+  const cellBtn3 = document.getElementById("cell-3");
+  const cellBtn4 = document.getElementById("cell-4");
+  const cellBtn5 = document.getElementById("cell-5");
+  const cellBtn6 = document.getElementById("cell-6");
+  const cellBtn7 = document.getElementById("cell-7");
+  const cellBtn8 = document.getElementById("cell-8");
+  const cellBtn9 = document.getElementById("cell-9");
+
   // Get random choice from computer
   const getComputerEasyMode = (min = 1, max = 9) => {
     console.log("Getting computer choice");
@@ -123,15 +133,6 @@ function GetChoice() {
     });
 
   async function handlePlayerChoice() {
-    const cellBtn1 = document.getElementById("cell-1");
-    const cellBtn2 = document.getElementById("cell-2");
-    const cellBtn3 = document.getElementById("cell-3");
-    const cellBtn4 = document.getElementById("cell-4");
-    const cellBtn5 = document.getElementById("cell-5");
-    const cellBtn6 = document.getElementById("cell-6");
-    const cellBtn7 = document.getElementById("cell-7");
-    const cellBtn8 = document.getElementById("cell-8");
-    const cellBtn9 = document.getElementById("cell-9");
     let result;
 
     try {
@@ -166,6 +167,15 @@ function GetChoice() {
     getComputerMediumMode,
     getComputerHardMode,
     handlePlayerChoice,
+    cellBtn1,
+    cellBtn2,
+    cellBtn3,
+    cellBtn4,
+    cellBtn5,
+    cellBtn6,
+    cellBtn7,
+    cellBtn8,
+    cellBtn9,
   };
 }
 
@@ -188,13 +198,34 @@ function PlayGame() {
     const getComputerHardMode = () => getChoice.getComputerHardMode();
     const handlePlayerChoice = () =>
       new Promise((resolve) => resolve(getChoice.handlePlayerChoice()));
+    const cellBtn1 = getChoice.cellBtn1;
+    const cellBtn2 = getChoice.cellBtn2;
+    const cellBtn3 = getChoice.cellBtn3;
+    const cellBtn4 = getChoice.cellBtn4;
+    const cellBtn5 = getChoice.cellBtn5;
+    const cellBtn6 = getChoice.cellBtn6;
+    const cellBtn7 = getChoice.cellBtn7;
+    const cellBtn8 = getChoice.cellBtn8;
+    const cellBtn9 = getChoice.cellBtn9;
+
     let cell;
 
     getActivePlayer().name === "Player One"
       ? (cell = await handlePlayerChoice())
       : (cell = getComputerEasyMode());
     console.log(cell);
-    return { cell };
+    return {
+      cell,
+      cellBtn1,
+      cellBtn2,
+      cellBtn3,
+      cellBtn4,
+      cellBtn5,
+      cellBtn6,
+      cellBtn7,
+      cellBtn8,
+      cellBtn9,
+    };
   };
 
   // Assign row and column values
@@ -234,7 +265,20 @@ function PlayGame() {
       column = 2;
     }
     console.log(row, column);
-    return { cell, row, column };
+    return {
+      cell,
+      row,
+      column,
+      cellBtn1: getCell.cellBtn1,
+      cellBtn2: getCell.cellBtn2,
+      cellBtn3: getCell.cellBtn3,
+      cellBtn4: getCell.cellBtn4,
+      cellBtn5: getCell.cellBtn5,
+      cellBtn6: getCell.cellBtn6,
+      cellBtn7: getCell.cellBtn7,
+      cellBtn8: getCell.cellBtn8,
+      cellBtn9: getCell.cellBtn9,
+    };
   };
 
   // Check if cell has already been selected
@@ -243,10 +287,38 @@ function PlayGame() {
     const cell = getCellRowColumn.cell;
     const row = getCellRowColumn.row;
     const column = getCellRowColumn.column;
+    const cellBtn1 = getCellRowColumn.cellBtn1;
+    const cellBtn2 = getCellRowColumn.cellBtn2;
+    const cellBtn3 = getCellRowColumn.cellBtn3;
+    const cellBtn4 = getCellRowColumn.cellBtn4;
+    const cellBtn5 = getCellRowColumn.cellBtn5;
+    const cellBtn6 = getCellRowColumn.cellBtn6;
+    const cellBtn7 = getCellRowColumn.cellBtn7;
+    const cellBtn8 = getCellRowColumn.cellBtn8;
+    const cellBtn9 = getCellRowColumn.cellBtn9;
 
     if (printBoard()[row][column] !== 0) {
       console.log(`Cell ${cell} has already been chosen`);
     } else {
+      if (cell === 1) {
+        cellBtn1.textContent = getActivePlayer().token;
+      } else if (cell === 2) {
+        cellBtn2.textContent = getActivePlayer().token;
+      } else if (cell === 3) {
+        cellBtn3.textContent = getActivePlayer().token;
+      } else if (cell === 4) {
+        cellBtn4.textContent = getActivePlayer().token;
+      } else if (cell === 5) {
+        cellBtn5.textContent = getActivePlayer().token;
+      } else if (cell === 6) {
+        cellBtn6.textContent = getActivePlayer().token;
+      } else if (cell === 7) {
+        cellBtn7.textContent = getActivePlayer().token;
+      } else if (cell === 8) {
+        cellBtn8.textContent = getActivePlayer().token;
+      } else if (cell === 9) {
+        cellBtn9.textContent = getActivePlayer().token;
+      }
       gameController.playTurn(cell, row, column);
     }
   };
